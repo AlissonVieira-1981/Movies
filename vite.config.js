0599d2699e60fdb1226235378dc09d79d01416f8
Alisson Vieira <alissonvieira@gmail.com>
2026-05-26 23:16:41 -0300

    arquivo vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/filmes-app/',
   plugins: [react()],
-  base: '/Movies/' // nome exato do repositório
 })
